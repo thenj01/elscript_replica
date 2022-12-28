19ef2ee5b2972a23533151389dbced3309483395
thenj01 <106470792+thenj01@users.noreply.github.com>
2022-12-28 13:02:56 +0545

    hello hello

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,9 @@ module.exports = {
   ],
     theme: {
       extend: {
-        colors :{
-          'grass': '#3cb878',
-          'silver': '#6d6d6d',
-          'silicon': '#ededed',
-        },
-      },
     },
     variants: {},
     plugins: [],
   }
+}
   
